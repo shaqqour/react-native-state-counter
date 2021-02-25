8d6ba40f18ea76a69e688f8c8dd38d59cb8554ca
Shaqqour <mshaqqourm@gmail.com>
2021-02-25 13:34:55 -0800

    Removes Text and TextInput imports from App.js as it is not needed any more

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import Counter from './Counter';
 import Greeting from './Greeting';
 
